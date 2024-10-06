32b5247f5e8702ab298ad842d0070459c5ee5614
Kareem Otoum <kareem12otoum@gmail.com>
2024-10-06 21:01:29 +0100

    Added team showcase and enhanced navbar

--- index.js
+++ index.js
@@ -3,15 +3,70 @@ const app = express();
 const port = 3000;
 const posts = [];
 
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+const team = [ 
+    {
+        name: "Zaine Khizer",
+        role: "Team Principal",
+        image: ""
+    },
+    {
+        name: "Charlie Best",
+        role: "Technical Director",
+        image: ""
+    },
+    {
+        name: "Warwick Darden",
+        role: "Head of Design",
+        image: ""
+    },,
+    {
+        name: "Freddie James",
+        role: "Head of Suspension",
+        image: ""
+    },,
+    {
+        name: "Tom Fellows",
+        role: "Head of Powertrains",
+        image: ""
+    },,
+    {
+        name: "Harry Brown",
+        role: "Head of Aerodynamics",
+        image: ""
+    },,
+    {
+        name: "Tom Streatfield",
+        role: "Head of Chasis",
+        image: ""
+    },,
+    {
+        name: "Ollie Fish",
+        role: "Head of Electrical",
+        image: ""
+    },,
+    {
+        name: "Arthur Moucheron",
+        role: "Head of Operations",
+        image: ""
+    },,
+    {
+        name: "Pari Bhatt",
+        role: "Head of Media",
+        image: ""
+    },,
+    {
+        name: "Aditya Ramkumar",
+        role: "Social Secretary",
+        image: ""
+    },
+]
 
 app.get("/", (req, res) =>
 {
-    res.render("index.ejs");
+    res.render("index.ejs", { team});
 })
 
 app.listen(port, () =>
